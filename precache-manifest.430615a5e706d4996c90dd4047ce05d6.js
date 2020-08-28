f858a0dae101bae44473b4485edd47b33c9f7582
Zahangir-Kabir <zahangir.ithome@gmail.com>
2020-08-28 09:13:30 +0600

    Updates

---
+++ precache-manifest.430615a5e706d4996c90dd4047ce05d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ad2ba06466ba0b645126a9f47cf320",
+    "url": "/display-react/index.html"
+  },
+  {
+    "revision": "8fd645f896e5b51cfd41",
+    "url": "/display-react/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "6fa36f5fe81f1501ac95",
+    "url": "/display-react/static/css/main.c74bc736.chunk.css"
+  },
+  {
+    "revision": "8fd645f896e5b51cfd41",
+    "url": "/display-react/static/js/2.bbfc7a15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/display-react/static/js/2.bbfc7a15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fa36f5fe81f1501ac95",
+    "url": "/display-react/static/js/main.9e4ffe27.chunk.js"
+  },
+  {
+    "revision": "4882644f4da66c746d3e",
+    "url": "/display-react/static/js/runtime-main.ecafb552.js"
+  }
+]);
